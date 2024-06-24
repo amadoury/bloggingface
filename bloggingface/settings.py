86a1e33297ae4db1aecbf6f40eeb32175e83643b
Amadou Oury Diallo <amadou7oury@gmail.com>
2024-06-24 05:39:13 +0200

    add locale_paths

--- bloggingface/settings.py
+++ bloggingface/settings.py
@@ -150,3 +150,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOCALE_PATHS = 'blog/locale/'
